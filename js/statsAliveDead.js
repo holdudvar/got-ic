728d4f0b92358136967e2697c02d9d10b630b515
holdudvar <email@mail.com>
2018-11-29 20:38:19 +0100

    statsAliveDead.js simplified

--- js/statsAliveDead.js
+++ js/statsAliveDead.js
@@ -1,24 +1,15 @@
-var aliveResult = '';
-var deadResult = '';
 var alive = [];
 var dead = [];
-var workingAlive = '';
-var workingDead = '';
 for (var i = 0; i < gameOfThronesCharacters.length; i += 1) {
   if (gameOfThronesCharacters.hasOwnProperty(i)) {
     if (gameOfThronesCharacters[i].dead === true) {
-      workingDead = gameOfThronesCharacters[i].name;
-      dead.push(workingDead);
+      dead.push(gameOfThronesCharacters[i].name);
     } else {
-      workingAlive = gameOfThronesCharacters[i].name;
-      alive.push(workingAlive);
+      alive.push(gameOfThronesCharacters[i].name);
     }
   }
 }
 alive.sort();
 dead.sort();
-aliveResult = alive.join('<br>');
-deadResult = dead.join('<br>')
-
-document.querySelector('#alive').innerHTML = aliveResult;
-document.querySelector('#dead').innerHTML = deadResult;
+document.querySelector('#alive').innerHTML = alive.join('<br>');
+document.querySelector('#dead').innerHTML = dead.join('<br>');
