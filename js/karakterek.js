c6a257f1ed66ce52117aff237a73874e47cbf656
Guóth Kevin <gkevin94@gmail.com>
2018-12-01 20:38:28 +0100

    edit and save buttons are working

--- js/karakterek.js
+++ js/karakterek.js
@@ -5,18 +5,23 @@ var element = document.getElementById('remove-button');
 
 for (var i = 0; i < gameOfThronesCharacters.length; i++) {
   tableRow +=
-        `
+    `
     <tr class='gotCharactersTable'>
         <td class="name">${gameOfThronesCharacters[i].name}</td>    
         <td class="image"><image src="./${gameOfThronesCharacters[i].portrait}" alt="${gameOfThronesCharacters[i].name}"</td>
         <td class="house">${ifExist()}</td>
-        <td class="bio" contenteditable="false">${gameOfThronesCharacters[i].bio}</td>
-        <td class="edit-button" ><button>Edit</button></td>
-        <td class="remove-button" id="btnHide" onclick="deleteRow(this)"><button>Delete</button></td>   
+        <td class="bio" id="bio_cell${i}">${gameOfThronesCharacters[i].bio}</td>
+        <td class="edit-button"><button id="editButton${i}" onclick="editRow('${i}')">Edit</button><button id="saveButton${i}" onclick="saveRow(${i})">Save</button></td>
+        <td class="remove-button"><button onclick="deleteRow(this)">Delete</button></td>   
     </tr>
 `;
 }
 characterTable.innerHTML = tableRow;
+for (var k = 0; k < gameOfThronesCharacters.length; k++) {
+  var saveID = 'saveButton' + k;
+  document.getElementById(`${saveID}`).style.display = 'none';
+}
+
 
 function ifExist() {
   if (gameOfThronesCharacters[i].house !== undefined) {
@@ -25,8 +30,28 @@ function ifExist() {
   return '';
 }
 
+function editRow(no) {
+  var bio = document.getElementById('bio_cell' + no);
+  var bioData = bio.innerHTML;
+
+  bio.innerHTML = `<textarea id="bio_text${no}" class="textArea">${bioData}</textarea>`;
+  document.getElementById('editButton' + no).style.display = 'none';
+  document.getElementById('saveButton' + no).style.display = 'inline';
+}
+
+
 function deleteRow(r) {
-  var k = r.parentNode.rowIndex;
-  document.getElementById('characters').deleteRow(k);
+  var j = r.parentNode.parentNode.rowIndex;
+  document.getElementById('characters').deleteRow(j);
 }
 element.addEventListener('click', deleteRow);
+
+
+function saveRow(no) {
+  var bioVal = document.getElementById('bio_text' + no).value;
+
+  document.getElementById('bio_cell' + no).innerHTML = bioVal;
+
+  document.getElementById('saveButton' + no).style.display = 'none';
+  document.getElementById('editButton' + no).style.display = 'inline';
+}
